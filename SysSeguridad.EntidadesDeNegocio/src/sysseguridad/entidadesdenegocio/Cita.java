baca17b318c56e6f27bb76974aeff08fa79502b4
jacqueline cruz <jacquelinecruz1706@outlook.es>
2023-08-10 13:27:13 -0600

    clases mascota y cita

---
+++ SysSeguridad.EntidadesDeNegocio/src/sysseguridad/entidadesdenegocio/Cita.java
@@ -0,0 +1,101 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package sysseguridad.entidadesdenegocio;
+
+/**
+ *
+ * @author jacqu
+ */
+public class Cita {
+    private int Id;
+    private int IdMascota;
+    private int IdUsuario;
+    private String Fecha;
+    private String Diagnostico;
+    private String Direccion;
+    private String Propietario;
+    private String TipoCita;
+
+    public Cita() {
+    }
+
+    public Cita(int Id, int IdMascota, int IdUsuario, String Fecha, String Diagnostico, String Direccion, String Propietario, String TipoCita) {
+        this.Id = Id;
+        this.IdMascota = IdMascota;
+        this.IdUsuario = IdUsuario;
+        this.Fecha = Fecha;
+        this.Diagnostico = Diagnostico;
+        this.Direccion = Direccion;
+        this.Propietario = Propietario;
+        this.TipoCita = TipoCita;
+    }
+
+    public int getId() {
+        return Id;
+    }
+
+    public void setId(int Id) {
+        this.Id = Id;
+    }
+
+    public int getIdMascota() {
+        return IdMascota;
+    }
+
+    public void setIdMascota(int IdMascota) {
+        this.IdMascota = IdMascota;
+    }
+
+    public int getIdUsuario() {
+        return IdUsuario;
+    }
+
+    public void setIdUsuario(int IdUsuario) {
+        this.IdUsuario = IdUsuario;
+    }
+
+    public String getFecha() {
+        return Fecha;
+    }
+
+    public void setFecha(String Fecha) {
+        this.Fecha = Fecha;
+    }
+
+    public String getDiagnostico() {
+        return Diagnostico;
+    }
+
+    public void setDiagnostico(String Diagnostico) {
+        this.Diagnostico = Diagnostico;
+    }
+
+    public String getDireccion() {
+        return Direccion;
+    }
+
+    public void setDireccion(String Direccion) {
+        this.Direccion = Direccion;
+    }
+
+    public String getPropietario() {
+        return Propietario;
+    }
+
+    public void setPropietario(String Propietario) {
+        this.Propietario = Propietario;
+    }
+
+    public String getTipoCita() {
+        return TipoCita;
+    }
+
+    public void setTipoCita(String TipoCita) {
+        this.TipoCita = TipoCita;
+    }
+    
+    
+
+}
---
+++ SysSeguridad.EntidadesDeNegocio/src/sysseguridad/entidadesdenegocio/Mascota.java
@@ -0,0 +1,111 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package sysseguridad.entidadesdenegocio;
+import java.time.LocalDate;
+/**
+ *
+ * @author jacqu
+ */
+public class Mascota {
+    private int Id;
+    private int IdUsuario;
+    private String Nombre;
+    private String Sexo;
+    private String Edad;
+    private String Raza;
+    private String SenialesParticulares;
+    private String Especie;
+    private String Propietario;
+
+    public Mascota() {
+    }
+
+    public Mascota(int Id, int IdUsuario, String Nombre, String Sexo, String Edad, String Raza, String SenialesParticulares, String Especie, String Propietario) {
+        this.Id = Id;
+        this.IdUsuario = IdUsuario;
+        this.Nombre = Nombre;
+        this.Sexo = Sexo;
+        this.Edad = Edad;
+        this.Raza = Raza;
+        this.SenialesParticulares = SenialesParticulares;
+        this.Especie = Especie;
+        this.Propietario = Propietario;
+    }
+
+    public int getId() {
+        return Id;
+    }
+
+    public void setId(int Id) {
+        this.Id = Id;
+    }
+
+    public int getIdUsuario() {
+        return IdUsuario;
+    }
+
+    public void setIdUsuario(int IdUsuario) {
+        this.IdUsuario = IdUsuario;
+    }
+
+    public String getNombre() {
+        return Nombre;
+    }
+
+    public void setNombre(String Nombre) {
+        this.Nombre = Nombre;
+    }
+
+    public String getSexo() {
+        return Sexo;
+    }
+
+    public void setSexo(String Sexo) {
+        this.Sexo = Sexo;
+    }
+
+    public String getEdad() {
+        return Edad;
+    }
+
+    public void setEdad(String Edad) {
+        this.Edad = Edad;
+    }
+
+    public String getRaza() {
+        return Raza;
+    }
+
+    public void setRaza(String Raza) {
+        this.Raza = Raza;
+    }
+
+    public String getSenialesParticulares() {
+        return SenialesParticulares;
+    }
+
+    public void setSenialesParticulares(String SenialesParticulares) {
+        this.SenialesParticulares = SenialesParticulares;
+    }
+
+    public String getEspecie() {
+        return Especie;
+    }
+
+    public void setEspecie(String Especie) {
+        this.Especie = Especie;
+    }
+
+    public String getPropietario() {
+        return Propietario;
+    }
+
+    public void setPropietario(String Propietario) {
+        this.Propietario = Propietario;
+    }      
+}
+
+
+
